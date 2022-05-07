186380efe6d1191e38206ae97b4d044c60e62a5b
GeneralHawking18 <chunghoang0103@gmail.com>
2022-05-07 10:01:23 +0700

    Upload DVD.java

---
+++ Lab03/DigitalVideoDisc.java
@@ -0,0 +1,37 @@
+public class DigitalVideoDisc {
+
+	private String title;
+
+	private String category;
+
+	private String director;
+
+	private int length;
+
+	private int cost;
+
+	public DigitalVideoDisc() {
+
+	}
+
+	public String getTitle() {
+		return null;
+	}
+
+	public String getCategory() {
+		return null;
+	}
+
+	public String getDirector() {
+		return null;
+	}
+
+	public int getLength() {
+		return 0;
+	}
+
+	public int getCost() {
+		return 0;
+	}
+
+}
