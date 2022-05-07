6cb202bd86e18f1ea7c02cd4c62f73f052ea0dfd
GeneralHawking18 <chunghoang0103@gmail.com>
2022-05-08 00:13:45 +0700

    Add method overloading task

--- Lab03/Cart.java
+++ Lab03/Cart.java
@@ -16,19 +16,40 @@ public class Cart {
 		}
 	}
 	
+	public void addDigitalVideoDisc(DigitalVideoDisc[] dvdlist) {
+		for (int i = 0; i < dvdlist.length; i++) {
+			addDigitalVideoDisc(dvdlist[i]);
+			if (qtyOrdered == MAX_NUMBERS_ORDERED) {
+				addDigitalVideoDisc(dvdlist[i]); // Just print the message 'Full'
+				break;
+			}
+		}
+	}
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc disc, int amount) {
+		for (int i = 0; i < amount; i++) {
+			addDigitalVideoDisc(disc);
+			if (qtyOrdered == MAX_NUMBERS_ORDERED) {
+				addDigitalVideoDisc(disc); // Just print the message 'Full'
+				break;
+			}
+		}
+	}
+	
 	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
-		for (int i = 0; i < qtyOrdered, i++) {
+		for (int i = 0; i < qtyOrdered; i++) {
 			if (itemsOrdered[i].getTitle() == disc.getTitle()) {
 				sumCost -= itemsOrdered[i].getCost(); // subtract the cost of the removed item
 				itemsOrdered[i] = null;
 				qtyOrdered --;
 				int j = i;
 				
-				while (j != qtyOrdered - 1) {
+				while (j < qtyOrdered - 1) {
 					itemsOrdered[j] = itemsOrdered[j + 1];
 					itemsOrdered[j + 1] = null;
-				}
+					j++;
 				
+				}
 			}
 		}
 	}
@@ -37,5 +58,5 @@ public class Cart {
 		return sumCost;
 	}
 	
-	
+
 }
