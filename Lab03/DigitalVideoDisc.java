2e45be8b51038d0a1a43ae75e54f8eb90286c836
GeneralHawking18 <chunghoang0103@gmail.com>
2022-05-07 20:40:39 +0700

    Add a DVD feature

--- Lab03/DigitalVideoDisc.java
+++ Lab03/DigitalVideoDisc.java
@@ -1,37 +1,59 @@
 public class DigitalVideoDisc {
-
 	private String title;
-
 	private String category;
-
 	private String director;
-
 	private int length;
-
 	private int cost;
-
-	public DigitalVideoDisc() {
-
+	
+	public DigitalVideoDisc(String title) {
+		super(); //Actually it can be omitted in this block.
+		this.title = title;
 	}
+	
+	
+	public DigitalVideoDisc(String title, String category, int cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
+
+
+	public DigitalVideoDisc(String title, String category, String director, int cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.cost = cost;
+	}
+
+	
+	public DigitalVideoDisc(String title, String category, String director, int length, int cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+	}
+
 
 	public String getTitle() {
-		return null;
+		return title;
 	}
-
 	public String getCategory() {
-		return null;
+		return category;
 	}
-
 	public String getDirector() {
-		return null;
+		return director;
 	}
-
 	public int getLength() {
-		return 0;
+		return length;
+	}
+	public int getCost() {
+		return cost;
 	}
 
-	public int getCost() {
-		return 0;
-	}
+	
 
 }
