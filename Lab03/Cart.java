00b3d3e6dfafa37c780e17a066961290c59fd91b
GeneralHawking18 <chunghoang0103@gmail.com>
2022-05-07 22:47:14 +0700

    Restore previous version and fix some bugs

--- Lab03/Cart.java
+++ Lab03/Cart.java
@@ -9,29 +9,13 @@ public class Cart {
 		if (qtyOrdered == MAX_NUMBERS_ORDERED) {
 			System.out.println("This cart is almost full");
 		} else {
-			qtyOrdered  ++;
 			itemsOrdered[qtyOrdered] = disc;
+			qtyOrdered++;
 			System.out.println("This disc has been added");
 			sumCost += disc.getCost(); // plus the cost of the new added item
 		}
 	}
 	
-	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
-		for (int i = 0; i < qtyOrdered, i++) {
-			if (itemsOrdered[i].getTitle() == disc.getTitle()) {
-				sumCost -= itemsOrdered[i].getCost(); // subtract the cost of the removed item
-				itemsOrdered[i] = null;
-				qtyOrdered --;
-				int j = i;
-				
-				while (j != qtyOrdered - 1) {
-					itemsOrdered[j] = itemsOrdered[j + 1];
-					itemsOrdered[j + 1] = null;
-				}
-				
-			}
-		}
-	}
 	
 	public float TotalCost() {
 		return sumCost;
