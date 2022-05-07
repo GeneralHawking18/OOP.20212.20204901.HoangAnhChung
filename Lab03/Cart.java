4c51cf22f6af2313ef18d7c3fe12d73fac4dd543
GeneralHawking18 <chunghoang0103@gmail.com>
2022-05-07 10:03:37 +0700

    Upload Cart.java into branch

---
+++ Lab03/Cart.java
@@ -0,0 +1,15 @@
+public class Cart {
+
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+
+	}
+
+	public void removeDigitalVideoDisc(DigitalVideoDisc dis) {
+
+	}
+
+	public int totalCost() {
+		return 0;
+	}
+
+}
