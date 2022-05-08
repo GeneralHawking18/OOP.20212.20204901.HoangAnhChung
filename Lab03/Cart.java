0e38c02ae9e096129db112951ae77328ced6b2b8
GeneralHawking18 <chunghoang0103@gmail.com>
2022-05-08 16:11:04 +0700

    Add remove-item feature

--- Lab03/Cart.java
+++ Lab03/Cart.java
@@ -15,6 +15,31 @@ public class Cart {
 			totalCost += disc.getCost(); // plus the cost of the new added item
 		}
 	}
+
+	public int removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		int qtyOrderedBackup = qtyOrdered;
+		DigitalVideoDisc[] Temp_itemsOrdered = itemsOrdered;
+		
+		for (int i = 0; i < qtyOrderedBackup; i++) {
+			if (itemsOrdered[i].getTitle() == disc.getTitle()) {
+				totalCost -= itemsOrdered[i].getCost(); // subtract the cost of the removed item
+				Temp_itemsOrdered[i] = null;
+				qtyOrdered --;
+				
+			}
+			itemsOrdered[i] = null; // be empty all slots in itemsOrdered
+		}
+		int j = 0;
+		
+		for (int i = 0; i < MAX_NUMBERS_ORDERED; i++) {
+			if (Temp_itemsOrdered[i] != null) {
+				itemsOrdered[j] = Temp_itemsOrdered[i]; // Refill the undeleted items
+				j ++;
+			}
+		}
+		
+		return qtyOrdered - qtyOrderedBackup;
+	}
 	
 	public float TotalCost() {
 		return totalCost;
