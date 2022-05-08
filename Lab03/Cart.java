bfdde5f86e5013d11c70cc94ba720827391a41a6
GeneralHawking18 <chunghoang0103@gmail.com>
2022-05-08 15:33:17 +0700

    Add cart feature

---
+++ Lab03/Cart.java
@@ -0,0 +1,24 @@
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	private int qtyOrdered = 0;
+	private float totalCost = 0;
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if (qtyOrdered == MAX_NUMBERS_ORDERED) {
+			System.out.println("This cart is almost full");
+		} else {
+			itemsOrdered[qtyOrdered] = disc;
+			qtyOrdered ++;
+			System.out.println("This disc has been added");
+			totalCost += disc.getCost(); // plus the cost of the new added item
+		}
+	}
+	
+	public float TotalCost() {
+		return totalCost;
+	}
+	
+
+}
