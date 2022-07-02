7cd09e6f169f99bd14fc2dc4c1e077b1812ba9b9
GeneralHawking18 <chunghoang0103@gmail.com>
2022-07-02 10:10:13 +0700

    Upload workclass

--- AimsProject/src/hust/soict/dsai/program/javafx/Painter.java
+++ AimsProject/src/hust/soict/dsai/program/javafx/Painter.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Painter extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("hust/soict/dsai/program/javafx/Painter.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("/hust/soict/dsai/program/javafx/Painter.fxml"));
 		
 		Scene scene = new Scene(root);
 		
